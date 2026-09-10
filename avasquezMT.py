95cff0c3b97c4832a8ca83a22b8bc770645d41f9
AV <angela_vasquez@upland.k12.ca.us>
2026-09-10 00:17:17 +0000

    lab MT

---
+++ avasquezMT.py
@@ -0,0 +1,15 @@
+#Angela Vasquez
+#Midterm Program #1
+#CTC 389
+
+#Ask a user to enter a number and then add to it.
+
+name = input("Please enter your name. ")
+
+number = int(input("Please enter a number. "))
+
+plus = int(input("How much would you like to have added to your number? "))
+
+total = number + plus
+
+print(name, "you entered", number, "and added", plus, "to get a total of", total)
